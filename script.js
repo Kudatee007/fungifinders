f788a3af6255c8ae9a4e9f6edbd492d17155178d
kudaT <timilehinkudaisi01@gmail.com>
2025-02-26 05:08:58 -0800

    Added google fonts

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const toggleBtn = document.querySelector(".hamburger");
 const dropDown = document.querySelector(".nav-links");
 
 toggleBtn.onclick = function () {
-    dropDown.classList.toggle("active");
-}
+  dropDown.classList.toggle("active");
+};
